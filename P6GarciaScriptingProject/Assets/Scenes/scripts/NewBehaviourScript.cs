65f0597ae24cea4c2f548cb18a84c2cb49616b56
751966-code <751966@student.dusd.net>
2025-11-10 13:16:07 -0800

    if statements completed

---
+++ P6GarciaScriptingProject/Assets/Scenes/scripts/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    //create an integer variable
+    int myInt = 5:
+
+    //Start is called before the first frame update 
+   void start()
+    {
+        //run the variable through my function 
+        myInt = MultiplyByTwo(myInt);
+
+        //display the result in the consle
+        Debug.Log(myInt);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+     (
+
+
+     }
+     //creating my function 
+     int MultiplyByTwo(int number)
+    //creating variable
+        int result;
+result = Number 2;
+}
